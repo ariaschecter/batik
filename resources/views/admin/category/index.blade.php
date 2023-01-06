20e35606c36f59eff3b9e423da54f6ff7ddad924
ariaschecter <aria.maulana.m@gmail.com>
2023-01-06 11:26:06 +0700

    [PATCH] Category Add :+1:

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">All {{ $city->city_name }} Category</h4>
+                    <h4 class="mb-sm-0">{{ $city->city_name }} City</h4>
                 </div>
             </div>
         </div>
