f2a319af89081a57b788580d17c2e0ab78f88c39
ariaschecter <aria.maulana.m@gmail.com>
2023-01-17 10:45:57 +0700

    [PATCH] Delete Unused Code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,30 +10,6 @@
 
     <!-- Banner Section -->
     <section class="banner-section">
-        {{-- <div class="container py-5">
-            <div class="main-timeline-2">
-                @php($i = 1)
-                @foreach ($categories as $category)
-                    <?php
-                        $batik = collect($category->batik_most);
-                    ?>
-                    @if($i % 2 == 1)
-                    <div class="timeline-2 left-2">
-                    @else
-                    <div class="timeline-2 right-2">
-                    @endif
-                        <div class="card">
-                        <img src="{{ asset('storage/' . $category->batik_most[0]->batik_picture) }}" class="card-img-top"
-                            alt="Responsive image">
-                        <div class="card-body p-4">
-                            <a href="{{ route('frontend.category.index', [$category->category_slug]) }}"><h4 class="fw-bold mb-4">{{ $category->category_name }}</h4></a>
-                        </div>
-                        </div>
-                    </div>
-                    @php($i++)
-                @endforeach
-            </div>
-        </div> --}}
         <section class="cd-h-timeline js-cd-h-timeline margin-bottom-md">
             <div class="cd-h-timeline__container container">
               <div class="cd-h-timeline__dates">
