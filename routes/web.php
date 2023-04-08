17e0e535f19402e435413b50abbdc51598db3cc4
ariaschecter <aria.maulana.m@gmail.com>
2023-04-08 23:04:32 +0700

    remove unused route

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::controller(HomeController::class)->group(function () {
     Route::get('/kota', 'city_index')->name('frontend.city.index');
     Route::get('/kota/{city:city_slug}', 'city_show_timeline')->name('frontend.timeline.show');
     Route::get('/category/{category:category_slug}', 'category_index')->name('frontend.category.index');
-    Route::get('/subcategory/{subcategory:sub_slug}', 'subcategory_index')->name('frontend.subcategory.index');
+    // Route::get('/subcategory/{subcategory:sub_slug}', 'subcategory_index')->name('frontend.subcategory.index');
     Route::get('/batik', 'batik_index')->name('frontend.batik.index');
     Route::get('/batik/{batik:batik_slug}', 'batik_show')->name('frontend.batik.show');
 });
